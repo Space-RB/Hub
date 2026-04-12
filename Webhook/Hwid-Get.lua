1fbd89386d48c8392b2e1ef32cb2e48deae1c331
Alexey <168357108+ago106@users.noreply.github.com>
2026-04-12 16:41:44 +0700

    Webhook Protector 2026-04-12 16:41

--- Webhook/Hwid-Get.lua
+++ Webhook/Hwid-Get.lua
@@ -1,4 +1,4 @@
-local webhookUrl = "https://discord.com/api/webhooks/1492821746330632287/vp4jAQAWDnd0byGFfzEKiRIEz093PO_MyKqqHb_ScvLLAESYrQ_6j2XWd7P6xHEZryd5"
+local webhookUrl = "https://discord.com/api/webhooks/1492822012639580200/p3e8fKiQ1Zgpbvq3QeCTFeIsRnL23p7_uTnCQJynwUQcBc1NLZbVZzrjACm1_kBtOOtK"
 
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
