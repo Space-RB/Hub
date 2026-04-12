7c805cdb40215c1515e0f27b44322c8af7adc717
Alexey <168357108+ago106@users.noreply.github.com>
2026-04-12 13:47:43 +0700

    Update AntiAFK.lua

--- Utility/AntiAFK.lua
+++ Utility/AntiAFK.lua
@@ -8,7 +8,7 @@ LP.Idled:Connect(function();
     VU:ClickButton2(Vector2.new(0,0));
     _G.log("print","Anti-AFK prevented kick :D");
     if _G.Settings.Debug == true and _G.NotificationLib and _G.NotificationLib.Notify then
-        pcall(function(); _G.NotificationLib.Notify("3","Anti AFK","Enabled","Info"); end);
+        pcall(function(); _G.Notify("Info","Anti AFK","Enabled",3); end);
     end;
 end);
 
