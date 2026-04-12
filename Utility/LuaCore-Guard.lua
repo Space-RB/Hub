89a926267bd2b2f4fd4678ba17ebc7fec1872524
Alexey <168357108+ago106@users.noreply.github.com>
2026-04-12 13:27:41 +0700

    Create LuaCore-Guard.lua

---
+++ Utility/LuaCore-Guard.lua
@@ -0,0 +1,6 @@
+local LuaCore = {
+   Protect = "2g7x7vx6enp3ub9er7b0m8b0bq8k0dnx",
+   Api = "55c74047efde281ffc47fd5f6fde5fb7",
+   Id = "1096fb26-faf0-4896-97a3-28d9a1d947c8",
+};
+return LuaCore;
