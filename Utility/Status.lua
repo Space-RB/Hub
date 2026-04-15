4f3008bccdc16405f978d04a2b714ae5ee1550a4
Alexey <168357108+ago106@users.noreply.github.com>
2026-04-15 19:55:01 +0700

    Create Status.lua

---
+++ Utility/Status.lua
@@ -0,0 +1,13 @@
+local Status = "Maintenance" -- Active/Maintenance
+local DiscordLink = "discord.gg/spacerb"
+
+print("[Space Hub]: " .. Status)
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+if Status == "Maintenance" then
+    local kickMessage = string.format("Space Hub is currently under maintenance.\nPlease check back later.\n\n%s", DiscordLink)
+    LocalPlayer:Kick(kickMessage)
+    return
+end
