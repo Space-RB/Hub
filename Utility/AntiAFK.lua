536a5d49db0651351202814165f016754c2da05e
Alexey <168357108+ago106@users.noreply.github.com>
2026-04-19 16:19:46 +0700

    Update AntiAFK.lua

--- Utility/AntiAFK.lua
+++ Utility/AntiAFK.lua
@@ -1,15 +1,23 @@
-local VU = game:GetService("VirtualUser");
-local LP = game:GetService("Players").LocalPlayer;
+local VU = game:GetService("VirtualUser")
+local LP = game:GetService("Players").LocalPlayer
 
-_G.log = _G.log or function(t,m) (t=="warn" and warn or print)("[Space Hub]: "..tostring(m)); end;
+_G.log = _G.log or function(t,m) 
+    if t == "warn" then
+        warn("[Space Hub]: "..tostring(m))
+    else
+        print("[Space Hub]: "..tostring(m))
+    end
+end
 
-LP.Idled:Connect(function();
-    VU:CaptureController();
-    VU:ClickButton2(Vector2.new(0,0));
-    _G.log("print","Anti-AFK prevented kick :D");
-    if _G.Settings.Debug == true and _G.NotificationLib and _G.NotificationLib.Notify then
-        pcall(function(); _G.Notify("Info","Anti AFK","Enabled",3); end);
-    end;
-end);
+LP.Idled:Connect(function()
+    VU:CaptureController()
+    VU:ClickButton2(Vector2.new(0,0))
+    _G.log("print","Anti-AFK prevented kick :D")
+    if _G.Settings and _G.Settings.Debug == true and _G.Notify then
+        pcall(function() 
+            _G.Notify("Info","Anti AFK","Anti-AFK is active!",3) 
+        end)
+    end
+end)
 
-_G.log("print","Anti-AFK Enabled");
+_G.log("print","Anti-AFK Enabled")
