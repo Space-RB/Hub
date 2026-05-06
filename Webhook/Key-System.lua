dc1f639aaa51d7d65159844ec820187d174833fc
Alexey <168357108+ago106@users.noreply.github.com>
2026-05-07 03:15:03 +0700

    Webhook Protector 2026-05-07 02:15

--- Webhook/Key-System.lua
+++ Webhook/Key-System.lua
@@ -1,4 +1,4 @@
-local WebhookURL = "https://discord.com/api/webhooks/1501676399671378092/4mKifLsCIWoJXgoedbgnpM5bC0aImmFGz1Rha-nCP2eTErCvgcPkEmwrzVqbGheOyKSQ"
+local WebhookURL = "https://discord.com/api/webhooks/1501678703711752252/460AhEwEQc5NfYkBIoYZshyE0tgDbeEZ5Z9iYkqwUyN_V4Cai8uRevhPb79-DBiF863g"
 
 local HttpService = game:GetService("HttpService")
 local UserInputService = game:GetService("UserInputService")
