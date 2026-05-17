afb5ebf90a56778296a3aff750a4c24a2d7c449f
Alexey <168357108+ago106@users.noreply.github.com>
2026-05-18 04:21:43 +0700

    Webhook Protector 2026-05-18 03:21

--- Webhook/LuaCore-Done.lua
+++ Webhook/LuaCore-Done.lua
@@ -1,4 +1,4 @@
-local webhookUrl = "https://discord.com/api/webhooks/1505679085433655336/-FWPznZhKEUonaHx4zxZasG0mWvnQaRnONBYL0Sz_yphaApLMsmJOd8Ub_VUBM-HNaPz";
+local webhookUrl = "https://discord.com/api/webhooks/1505681746308497418/kCuDtw8meY9pQBa5GLqPPERVfwKjh6eM0K-K3bzKtx-I1vW7v9vhJ8NK6esasLUz52-t";
 
 local HttpService = game:GetService("HttpService");
 local Players = game:GetService("Players");
