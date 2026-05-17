1b46e34b0075be4f29afde30155769a885fdc9c7
Alexey <168357108+ago106@users.noreply.github.com>
2026-05-17 23:19:17 +0700

    Webhook Protector 2026-05-17 22:19

--- Webhook/MM2.lua
+++ Webhook/MM2.lua
@@ -1,1 +1,1 @@
-return "https://discord.com/api/webhooks/1505413930728554566/PT5yJQ5qp4eItMlUA9EXUsIuzf4A658aZBODSP-LX7PgDFpjzSQnrNB7SWOPVZQlsIeJ"
+return "https://discord.com/api/webhooks/1505605636304408709/3IAlk_iJef5yAEfBZTgs7LuD-mr0oI21Cqp-eDvYSeMFuSPmvfkWU4ea-YuCxz2JvNbp"
