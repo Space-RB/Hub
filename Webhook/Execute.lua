d019dd47893abda69b7eeab29b55d1e04036468a
Alexey <168357108+ago106@users.noreply.github.com>
2026-05-20 04:01:37 +0700

    Webhook Protector 2026-05-20 03:01

--- Webhook/Execute.lua
+++ Webhook/Execute.lua
@@ -1,4 +1,4 @@
-local webhookUrl = "https://discord.com/api/webhooks/1506401153414336752/eJia0orEe1PvZAdidRtK9kw2E3UOAJZNYqCW1OlnulhzBmrchH6AKEJVyd5LD-IbTr8l";
+local webhookUrl = "https://discord.com/api/webhooks/1506401460970324041/5yFxd1SHe9lYt9FJIUx6x13DJc7hhl-IMHwYzmItct1luQCHf_9DtxtIREyL99vxLrwu";
 
 local HttpService = game:GetService("HttpService");
 
