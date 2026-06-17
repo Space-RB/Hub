59ed29d083bd5d577c71843106335f2eb7e08331
Alexey <168357108+ago106@users.noreply.github.com>
2026-06-17 18:14:55 +0700

    Webhook Protector 2026-06-17 16:14

--- Webhook/BABFT.lua
+++ Webhook/BABFT.lua
@@ -1,1 +1,1 @@
-return "https://discord.com/api/webhooks/1507095450103316650/cgdPS3AHwrcs_JUdBt9gmoZSVIoPkE1n43-eVU0frvAvQ-_fGgP3oMKDwc4P37h5q-7O"
+return "https://discord.com/api/webhooks/1516763063502503996/UIlvQ43B9zsIHQNnzgMweuGZ2tFkarDw-zef2YfT3MJB972tyDqcVlq6TMa-iPbsFY-2"
